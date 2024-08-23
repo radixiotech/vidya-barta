8999dcce014683811e30fd1d324dac99d92198be
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2024-08-23 14:53:23 +0530

    - Initialize shadcn ui for our project. - Use custom ui components in vidya-barta frontend.

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,7 +4,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
 });
