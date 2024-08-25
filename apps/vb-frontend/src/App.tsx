3c0d043ca814a65b4a3363a0240770ed98628ba7
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2024-08-25 12:44:06 +0530

    Added dialog component.

--- apps/vb-frontend/src/App.tsx
+++ apps/vb-frontend/src/App.tsx
@@ -1,7 +1,33 @@
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@vidya-barta/ui';
+import { useState } from 'react';
+
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <h1>Vidya Barta</h1>
+      <Dialog open={open} defaultOpen={open} onOpenChange={setOpen}>
+        <DialogTrigger>
+          <Button onClick={() => setOpen(true)}>Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
